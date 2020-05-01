fab20bd9a98b1f644e64c65170a7c5d02282ee95
DARKSIDE08 <jlega2thekillers@hotmail.com>
2020-05-01 18:57:58 -0500

    Nuevo repositorio

---
+++ WALL-W/WALL-W.ino
@@ -0,0 +1,15 @@
+void setup() {
+ /// aquivan las variables
+
+}
+void mifuncion(){
+  int alpha=12;
+  //nueva variable
+}
+void loop() {
+  // Sentencias de control
+  //hola como estas
+  
+  
+
+}
